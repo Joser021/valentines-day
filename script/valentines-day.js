6b6ec8f88ecab4e349dfaea865dfcffef9b325d3
joser021 <joser.dev021@gmail.com>
2025-06-11 22:25:17 -0300

    finished

--- script/valentines-day.js
+++ script/valentines-day.js
@@ -104,3 +104,26 @@ function slider() {
 }
 
 setInterval(slider, 5000);
+
+// compare time
+const calculateTime = setInterval(function time() {
+    const time = document.querySelector(".dateTime")
+    let today = new Date();
+    const firstDate = new Date("2024-12-10");
+    let comparing = today - firstDate;
+
+    let years = Math.floor(comparing / (1000 * 60 * 60 * 24 * 365.25));
+    let months = Math.floor(comparing / (1000 * 60 * 60 * 24 * 30.4375)) % 12;
+    let days = Math.floor(comparing / (1000 * 60 * 60 * 24)) % 30;
+    let hours = Math.floor(comparing / (1000 * 60 * 60)) % 24;
+    let minutes = Math.floor(comparing / (1000 * 60)) % 60;
+    let seconds = Math.floor(comparing / (1000)) % 60;
+    
+    if (years < 10) years = "0" + years
+    if (months < 10) months = "0" + months
+    if (days < 10) days = "0" + days
+    if (hours < 10) hours = "0" + hours
+    if (minutes < 10) minutes = "0" + minutes
+    if (seconds < 10) seconds = "0" + seconds
+    time.innerHTML = `Amando você por <span class="date">${years}</span> Anos <span class="date">${months}</span> Meses <span class="date">${days}</span> Dias <span class="date">${hours}</span> Horas <span class="date">${minutes}</span> Minutos <span class="date">${seconds}</span> Segundos`
+})
